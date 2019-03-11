a8f8bf7740b46be8aaa04bc223fa879d2fcbc6f6
Mike Singarayar <michaelsingarayar@gmail.com>
2019-03-10 23:06:48 -0600

    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.0422aeb1053a1c28bd72222cd705a58d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1850744144eed8a4a90a23f280c2662",
+    "url": "/The-Office-Click-App/static/media/pam.b1850744.jpg"
+  },
+  {
+    "revision": "8ba54c284f060c790f75",
+    "url": "/The-Office-Click-App/static/css/main.2aa0ceb9.chunk.css"
+  },
+  {
+    "revision": "69d334036c900dbd7031",
+    "url": "/The-Office-Click-App/static/js/runtime~main.2b91a89d.js"
+  },
+  {
+    "revision": "ef6d01884dbf155f1eff",
+    "url": "/The-Office-Click-App/static/js/2.75bec7cd.chunk.js"
+  },
+  {
+    "revision": "eb621799fc4e486e68293ccf67f2db79",
+    "url": "/The-Office-Click-App/static/media/jim.eb621799.jpg"
+  },
+  {
+    "revision": "9068b9442f9f493cdeb6de6a5fc55517",
+    "url": "/The-Office-Click-App/static/media/michael.9068b944.jpg"
+  },
+  {
+    "revision": "8ba54c284f060c790f75",
+    "url": "/The-Office-Click-App/static/js/main.d498ed8b.chunk.js"
+  },
+  {
+    "revision": "3b0cbc2d26e53d92295daceb3bb2dbb3",
+    "url": "/The-Office-Click-App/static/media/Angela.3b0cbc2d.jpg"
+  },
+  {
+    "revision": "e99d832f5f2bb541a7d0d7c0bdd262db",
+    "url": "/The-Office-Click-App/static/media/toby.e99d832f.jpg"
+  },
+  {
+    "revision": "35406b7f751a9b5011967ce376538ee7",
+    "url": "/The-Office-Click-App/static/media/dwight.35406b7f.jpg"
+  },
+  {
+    "revision": "ab845e0ec8d68b4eb8590db76b1c485f",
+    "url": "/The-Office-Click-App/static/media/andy.ab845e0e.jpg"
+  },
+  {
+    "revision": "561de699897eaa14c86e597a36fc3df9",
+    "url": "/The-Office-Click-App/static/media/creed.561de699.jpg"
+  },
+  {
+    "revision": "a7f541b9af03c1c0e68399ce4d8e038e",
+    "url": "/The-Office-Click-App/index.html"
+  }
+];
