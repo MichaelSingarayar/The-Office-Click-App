4c5a53388f450bb886bc38306e7a14fe3fe55050
Mike Singarayar <michaelsingarayar@gmail.com>
2019-04-04 17:58:41 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/The-Office-Click-App/precache-manifest.e76e1634b78c9a1115b8099001809578.js"
+  "/precache-manifest.69ce498a8113fef513dd62cf3ea59cd1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/The-Office-Click-App/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
